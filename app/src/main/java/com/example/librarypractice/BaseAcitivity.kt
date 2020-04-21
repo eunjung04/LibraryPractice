6bf8b728faa951bcdcf51c890aacb37053a9be9f
eunjung04 <peunj04@gmail.com>
2020-04-21 21:06:46 +0900

    Initial commit

---
+++ app/src/main/java/com/example/librarypractice/BaseAcitivity.kt
@@ -0,0 +1,20 @@
+package com.example.librarypractice
+
+import android.content.Context
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseAcitivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValue()
+
+
+
+    }
+
+
+
