7f43697cc6fd7f55db6f5cb23ac7fa4094e200cb
eunjung04 <peunj04@gmail.com>
2020-04-21 21:44:18 +0900

    인터넷상의 이미지를 불러오는 글라이드 라이브러리 사용법

--- app/src/main/java/com/example/librarypractice/MainActivity.kt
+++ app/src/main/java/com/example/librarypractice/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.librarypractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseAcitivity() {
 
@@ -15,6 +17,10 @@ class MainActivity : BaseAcitivity() {
     }
 
     override fun setValue() {
+
+        Glide.with(mContext).load("https://pds.joins.com/news/component/htmlphoto_mmdata/201810/29/htm_2018102914510995660.jpeg").into(profileImg)
+
+
 
     }
 }
